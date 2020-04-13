66d9dc1b1fedfc89e63764745613bc724394055a
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-04-13 00:01:59 -0500

    css

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['assets/index.css']
 
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__)
 
 chick_data = {
     'id': ['1058'],
@@ -14,55 +14,65 @@ chick_data = {
     'time': ['06:54pm'],
     'gender':['female'],
     'image_name':['opencv_frame_452.png']
-}
+}     
 app.layout = html.Div([
         html.Div(id='header', children=[
-            html.H3(children=["PARVADA"], style={'color':'white'})],
-                 style={'background-color':'purple'}
-                 ),
-        html.Div(id='control-buttons', children=[
+            html.Div(className='header-container', children=[
+                html.H3(children=["PARVADA"])]
+                    )
+            ]),
+
+        html.Div(id='control-buttons', className='buttons-container', children=[
             dcc.RadioItems(
-                id='time_span_options',
+                id='time_span_options', className='buttons',
                 options=[{'label': 'Last batch','value': 'Last_batch'},
                          {'label': 'Last hour','value': 'Last_hour'}],
                 value= 'Last batch',
-                labelStyle={'display':'inline-block'},
-                )], style= {'textAlign':'right', 'padding-right':'13%', 'padding-bottom':'3%', 'padding-top':'1%'}
+                )]
         ),
-        html.Div(id='content-wrapper', children=[
-            html.Div(id='left-content-wrapper', children=[
-                html.Div(id='counters', children=[
-                    html.Div(id='female_counter', children=[
-                        html.Img(src='/assets/Icons/female.png', width='90%'),
-                        html.Strong(children=["5,500"], style={'left-padding':'49%'}),
-                        html.P("Females")],style={'width':'30%','text-align':'center','display':'inline-block'}),
-                    html.Div(id='male_counter',children=[
-                        html.Img(src='/assets/Icons/male.png', width='90%'),
-                        html.Strong(children=["5,502"], style={'left-padding':'49%'}),
-                        html.P("Males")],style={'width':'30%','text-align':'center','display':'inline-block'}),
-                    html.Div(id='counter',children=[
-                        html.Img(src='/assets/Icons/chick.png', width='80%'),
-                        html.Strong(children=["11,002"], style={'left-padding':'49%'}),
-                        html.P("Chicks")],style={'width':'30%','text-align':'center','display':'inline-block'})
-                ],style={'display':'inline-block','width':'100%','padding-bottom':'3%'}),
-                html.Img(src='/assets/Best_pic/best_pic_pollito.png', width='80%')
-            ], style={'width': '70%', 'display':'inline-block'}),
-            html.Div(id='sexed-chick-stream', children=[
+        html.Div(id='content-wrapper', className='container', children=[
+            html.Div(id='left-content-wrapper', className='main-container', children=[
+                html.Div(id='counters', className='boxes',children=[
+                    html.Div(id='female_counter', className='box female', children=[
+                        html.Strong(children=["5,500"]),
+                        html.P("Females"),
+                        html.Img(className= 'iconF', src='/assets/Icons/female.svg')]),
+                    html.Div(id='male_counter', className='box male', children=[
+                        html.Strong(children=["5,502"]),
+                        html.P("Males"),
+                        html.Img(className= 'iconM',src='/assets/Icons/male.svg')]),
+                    html.Div(id='counter', className='box chicken', children=[
+                        html.Strong(children=["11,002"]),
+                        html.P("Chicks"),
+                        html.Img(className= 'iconC',src='/assets/Icons/CHICKEN.svg')])
+                ]),
+            html.Div(className='video-container', children=[
+                html.Video(className= 'video', controls = True, id = 'movie_player'),
+            ]),    
+            ]),
+            html.Div(id='sexed-chick-stream', className='side-container', children=[
+                html.Div(className='recently', children=[
                 html.H6("Recently classified"),
-                html.Div(id='chick-1', children=[
-                    html.P("Today 03:45pm", style={'padding-right':'40%'}),
-                    html.Strong(children=["Female"], className='label-female', style={'padding-right':'70%'}),
-                    html.Img(src='/assets/Streaming/opencv_frame_475.png', style={'width': '100%'})
-                ],style={'width':'100%', 'padding-bottom':'8%', 'border-top':'1px solid purple'}),
-                 html.Div(id='chick-2', children=[
-                    html.P("Today 03:50pm", style={'padding-right':'40%'}),
-                    html.Strong(children=["Male"], className='label-male', style={'padding-right':'70%'}),
-                    html.Img(src='/assets/Streaming/opencv_frame_475.png', style={'width': '100%'})
-                ],style={'width':'100%', 'padding-bottom':'8%', 'border-top':'1px solid purple'})
+                ]),
+                html.Div(id='chick-1', className='gender', children=[
+                    html.P("Today 03:45pm"),
+                    html.Strong(children=["Female"], className='badge pink'),
+                    html.Img(src='/assets/Streaming/opencv_frame_475.png')
+                ]),
+                html.Div(id='chick-2', className='gender', children=[
+                    html.P("Today 03:50pm"),
+                    html.Strong(children=["Male"], className='badge blue'),
+                    html.Img(src='/assets/Streaming/opencv_frame_475.png')
+                ]),
+                html.Div(id='chick-3', className='gender', children=[
+                    html.P("Today 03:50pm"),
+                    html.Strong(children=["Male"], className='badge blue'),
+                    html.Img(src='/assets/Streaming/opencv_frame_475.png')
+                ])                     
                ]
-            ,style={'width': '28%', 'display':'inline-block', 'border-style':'solid', 'textAlign':'center','vertical-align':'top'})
-        ],style={'display':'inline-block','width':'80%','padding-left':'10%'})
-   ],style={'width':'100%'}
+            )
+        ])
+   ]
 )
 
 
@@ -73,15 +83,11 @@ app.layout = html.Div([
     [Input('countries-radio', 'value')])
 def set_cities_options(selected_country):
     return [{'label': i, 'value': i} for i in all_options[selected_country]]
-
-
 @app.callback(
     Output('cities-radio', 'value'),
     [Input('cities-radio', 'options')])
 def set_cities_value(available_options):
     return available_options[0]['value']
-
-
 @app.callback(
     Output('display-selected-values', 'children'),
     [Input('countries-radio', 'value'),
@@ -92,6 +98,8 @@ def set_display_children(selected_country, selected_city):
     )
 '''
 
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
