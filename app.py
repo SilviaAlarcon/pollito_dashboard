e831c0e70aa9e28d26eb5b39f931a7b4f3331ae5
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-04-18 17:40:05 -0500

    prueba

--- app.py
+++ app.py
@@ -15,65 +15,68 @@ chick_data = {
     'gender':['female'],
     'image_name':['opencv_frame_452.png']
 }     
-app.layout = html.Div([
-        html.Div(id='header', children=[
-            html.Div(className='header-container', children=[
-                html.H3(children=["PARVADA"])]
-                    )
-            ]),
+app.layout = html.Div(className= 'hero', children = [
+        html.Div(className= 'hero-container', children = [
+            html.Div(id='header', children=[
+                html.Div(className='header-container', children=[
+                    html.Img(className= 'logo', src='/assets/Icons/logo.jpeg')]
+                        )
+                ]),
 
-        html.Div(id='control-buttons', className='buttons-container', children=[
-            dcc.RadioItems(
-                id='time_span_options', className='buttons',
-                options=[{'label': 'Last batch','value': 'Last_batch'},
-                         {'label': 'Last hour','value': 'Last_hour'}],
-                value= 'Last batch',
-                )]
-        ),
-        html.Div(id='content-wrapper', className='container', children=[
-            html.Div(id='left-content-wrapper', className='main-container', children=[
-                html.Div(id='counters', className='boxes',children=[
-                    html.Div(id='female_counter', className='box female', children=[
-                        html.Strong(children=["5,500"]),
-                        html.P("Females"),
-                        html.Img(className= 'iconF', src='/assets/Icons/female.svg')]),
-                    html.Div(id='male_counter', className='box male', children=[
-                        html.Strong(children=["5,502"]),
-                        html.P("Males"),
-                        html.Img(className= 'iconM',src='/assets/Icons/male.svg')]),
-                    html.Div(id='counter', className='box chicken', children=[
-                        html.Strong(children=["11,002"]),
-                        html.P("Chicks"),
-                        html.Img(className= 'iconC',src='/assets/Icons/CHICKEN.svg')])
-                ]),
-            html.Div(className='video-container', children=[
-                html.Video(className= 'video', controls = True, id = 'movie_player'),
-            ]),    
-            ]),
-            html.Div(id='sexed-chick-stream', className='side-container', children=[
-                html.Div(className='recently', children=[
-                html.H6("Recently classified"),
-                ]),
-                html.Div(id='chick-1', className='gender', children=[
-                    html.P("Today 03:45pm"),
-                    html.Strong(children=["Female"], className='badge pink'),
-                    html.Img(src='/assets/Streaming/opencv_frame_475.png')
-                ]),
-                html.Div(id='chick-2', className='gender', children=[
-                    html.P("Today 03:50pm"),
-                    html.Strong(children=["Male"], className='badge blue'),
-                    html.Img(src='/assets/Streaming/opencv_frame_475.png')
-                ]),
-                html.Div(id='chick-3', className='gender', children=[
-                    html.P("Today 03:50pm"),
-                    html.Strong(children=["Male"], className='badge blue'),
-                    html.Img(src='/assets/Streaming/opencv_frame_475.png')
-                ])                     
-               ]
-            )
+            html.Div(id='control-buttons', className='buttons-container', children=[
+                dcc.RadioItems(
+                    id='time_span_options', className='buttons',
+                    options=[{'label': 'Last batch','value': 'Last_batch'},
+                            {'label': 'Last hour','value': 'Last_hour'}],
+                    value= 'Last batch',
+                    )]
+            ),
+            
+                
+                    
+                        html.Div(id='female_counter', className='box-female', children=[
+                            html.Strong(children=["5,500"]),
+                            html.P("Females"),
+                            html.Img(className= 'iconF', src='/assets/Icons/female.svg')]),
+                        html.Div(id='male_counter', className='box-male', children=[
+                            html.Strong(children=["5,502"]),
+                            html.P("Males"),
+                            html.Img(className= 'iconM',src='/assets/Icons/male.svg')]),
+                        html.Div(id='counter', className='box-chicken', children=[
+                            html.Strong(children=["11,002"]),
+                            html.P("Chicks"),
+                            html.Img(className= 'iconC',src='/assets/Icons/CHICKEN.svg')]),
+                    
+                html.Div(className='video-container', children=[
+                    html.Video(className= 'video', controls = True, id = 'movie_player'),
+                ]),    
+                
+                html.Div(id='sexed-chick-stream', className='side-container', children=[
+                    html.Div(className='recently', children=[
+                    html.H6("Recently classified"),
+                    ]),
+                    html.Div(id='chick-1', className='gender', children=[
+                        html.P("Today 03:45pm"),
+                        html.Strong(children=["Female"], className='badge pink'),
+                        html.Img(src='/assets/Streaming/opencv_frame_475.png')
+                    ]),
+                    html.Div(id='chick-2', className='gender', children=[
+                        html.P("Today 03:50pm"),
+                        html.Strong(children=["Male"], className='badge blue'),
+                        html.Img(src='/assets/Streaming/opencv_frame_475.png')
+                    ]),
+                    html.Div(id='chick-3', className='gender', children=[
+                        html.P("Today 03:50pm"),
+                        html.Strong(children=["Male"], className='badge blue'),
+                        html.Img(src='/assets/Streaming/opencv_frame_475.png')
+                    ])                     
+                ]
+                )
+            
         ])
    ]
 )
+
 
 
 
